a08b34f6207f30968af9fb3972d2e49db42927ff
elias.jr.dev@gmail.com <elias.jr.dev@gmail.com>
2024-12-29 20:58:21 -0300

    Step 7 - JSON Model -  O 'M' do MVC

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,22 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], (Controller, MessageToast)=>{
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], (Controller, MessageToast, JSONModel)=>{
     "use strict"
 
     return Controller.extend("ui5.tutorial.controller.App",{
+        onInit(){
+            //set Data model on View
+            const oData = {
+                recipient:{
+                    name: "Camarada"
+                }
+            };
+            const oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
+
         onShowHello(){
             MessageToast.show("Fala MANO BROW!");
         }
