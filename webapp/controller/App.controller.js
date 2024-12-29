4b493c0968867f4f85f074b05a13dbcd17672af7
elias.jr.dev@gmail.com <elias.jr.dev@gmail.com>
2024-12-29 20:52:18 -0300

    Step 6 - Módulos = Recursos

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller)=>{
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller, MessageToast)=>{
     "use strict"
 
     return Controller.extend("ui5.tutorial.controller.App",{
         onShowHello(){
-            //Show a native Javascript Alert
-            alert("Fala Mano!");
+            MessageToast.show("Fala MANO BROW!");
         }
     });
 });
